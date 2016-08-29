08e34bd95e7d1f2642a91d5c09a8e7f3e24a3954
Jason Sutter <jasonsutter87@gmail.com>
2016-08-29 12:56:43 -0700

    Wrote tests for sort stack, and started peek/isempty methods

--- stacks_and_queues/stack_min.rb
+++ stacks_and_queues/stack_min.rb
@@ -5,5 +5,25 @@
 # => I would find the smallest number add the stack to the queue, then shift the numbers until the smallest is at the back
 # => Then re add the numbers back into the stack and pop off the last one until there is only one left.
 
-def higest_num_stack(array)
+def higest_num_stack(stack)
+	#if stack length is equal to 1 it will be the highest
+	return stack[0] if stack.length == 1
+	temp_queue = [] #set up the tempoary queue for sorting
+	smallest = stack.min #find the minimum number in the stack
+	
+	# loop until the stack is empty, add the elements to the queue
+	until stack.length == 0 
+		temp_queue << stack.pop
+	end
+	
+	#loop through the queue until the minimum number is on the end
+	until smallest == temp_queue.last
+		temp_queue.push(temp_queue.shift)
+	end
+
+	#the temporay queue becomes our new stack, and we pop off the end
+	stack = temp_queue
+	stack.pop
+	#continue until basecase
+	higest_num_stack(stack)
 end
---
+++ stacks_and_queues/stacks_and_queue_spec/sort_stack_rspec.rb
@@ -0,0 +1,17 @@
+require_relative('../sort_stack')
+
+describe 'It should sort the stack' do
+	let(:mix_stack) {[2,3,5,61,1,2,4,3]}
+	let(:empty_stack) {[]}
+	it 'it should stack with the smallest number on top' do
+		expect(sort_stack(mix_stack)).to eq([61,5,4,3,3,2,2,1])
+	end
+
+	it 'it should return with the smallest number of 1' do
+		expect(sort_stack(mix_stack).last).to eq(1)
+	end
+
+	it 'it should return nil if the stack is empty' do
+		expect(sort_stack(empty_stack).last).to eq(1)
+	end
+end
