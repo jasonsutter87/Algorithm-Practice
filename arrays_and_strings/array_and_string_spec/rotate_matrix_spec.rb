9f62af7befc544f16ec668ce5d7c26e8f2765aae
Jason Sutter <jasonsutter87@gmail.com>
2016-08-30 14:29:31 -0700

    Finished the reset of the specs

--- arrays_and_strings/array_and_string_spec/rotate_matrix_spec.rb
+++ arrays_and_strings/array_and_string_spec/rotate_matrix_spec.rb
@@ -1,7 +1,11 @@
 require_relative('../rotate_matrix')
 
-describe '' do
-	it '' do
-		
+describe 'turn an array 90 degrees to the right' do
+	let(:portrait){[["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]]}
+	let(:portrait_90cc)[["a", "d", "g"], ["b", "e", "h"], ["c", "f", "i"]]
+	
+
+	it 'it will return a new array that has been rotated' do
+		expect(rotate_matrix(portrait)).to eq(portrait_90cw)
 	end
 end
